30e09b450e25b6b8dddae8a6579a914df09d36b4
margaeor <margaeor@gmail.com>
2020-02-04 23:10:38 +0200

    Edit agent

--- dqn_agent/agent.py
+++ dqn_agent/agent.py
@@ -1,31 +1,28 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import numpy as np
-import keras.backend.tensorflow_backend as backend
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
-from keras.optimizers import Adam
-from keras.callbacks import TensorBoard
 import tensorflow as tf
+from tensorflow import keras
 from collections import deque
 import time
 import random
-from tqdm import tqdm
+
 import os
 from PIL import Image
 import cv2
 
-
 GAMMA = 0.99 # Parameter used to discount future rewards
-EXPERIENCE_REPLAY_SIZE = 50_000  # How many last steps to keep for model training
-MIN_EXPERIENCE_REPLAY_SIZE = 1_000  # Minimum number of steps in a memory to start training
+EXPERIENCE_REPLAY_SIZE = 50000  # How many last steps to keep for model training
+MIN_EXPERIENCE_REPLAY_SIZE = 1000  # Minimum number of steps in a memory to start training
 LEARNING_RATE = 0.001 # Adam optimizer learning rate
 MINIBATCH_SIZE = 64  # Size of training batch
 UPDATE_TARGET_EVERY = 5  # Update target every 5 episodes
-MODEL_NAME = '2x256'
+MODEL_NAME = 'first'
 MIN_REWARD = -200  # Minimum reward required to save the model
-MEMORY_FRACTION = 0.20
+GPU_MEMORY_FRACTION = 0.8 # Defines fraction of GPU memory used by tf
 
 # Number of training episodes
-EPISODES = 20_000
+EPISODES = 20000
 
 # Exploration settings
 epsilon = 1  # not a constant, going to be decayed
@@ -35,43 +32,16 @@ EPSILON_DECAY = 0.99975
 AGGREGATE_STATS_EVERY = 50  # episodes
 SHOW_PREVIEW = False
 
-class CustomTensorBoard(TensorBoard):
-    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.step = 1
-        self.writer = tf.summary.FileWriter(self.log_dir)
-
-    # Overriding this method to stop creating default log writer
-    def set_model(self, model):
-        pass
-
-    # Overrided, saves logs with our step number
-    # (otherwise every .fit() will start writing from 0th step)
-    def on_epoch_end(self, epoch, logs=None):
-        self.update_stats(**logs)
-
-    # Overrided
-    # We train for one batch only, no need to save anything at epoch end
-    def on_batch_end(self, batch, logs=None):
-        pass
-
-    # Overrided, so won't close writer
-    def on_train_end(self, _):
-        pass
-
-    # Custom method for saving own metrics
-    # Creates writer, writes custom metrics and closes writer
-    def update_stats(self, **stats):
-        #self._write_logs(stats, self.step)
-        pass
-
 
 class DQNAgent:
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, load_data=False):
 
         # Our main DQN model
-        self.model = self.create_model()
+        self.policy_model = self.create_model()
+
+        # Configure paths
+        self.log_dir = "logs/{}-{}".format(MODEL_NAME, int(time.time()))
+        self.checkpoint_path = "checkpoints/{}.ckpt".format(MODEL_NAME)
 
         # DQN network input and output size.
         # input_size corresponds to image dimensions and
@@ -81,37 +51,60 @@ class DQNAgent:
 
         # Target network used for smoother training
         self.target_model = self.create_model()
-        self.target_model.set_weights(self.model.get_weights())
+        self.target_model.set_weights(self.policy_model.get_weights())
 
         # An array with last n steps for training
         self.replay_memory = deque(maxlen=EXPERIENCE_REPLAY_SIZE)
 
-        # Custom tensorboard object
-        self.tensorboard = CustomTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
+        # Tensorboard object to keep logs and images
+        self.tensorboard = keras.callbacks.TensorBoard(
+            log_dir=self.log_dir,
+            update_freq='epoch'
+        )
+
+        # Checkpoint object used to save and retrieve our model upon request
+        self.cp_callback = keras.callbacks.ModelCheckpoint(
+            filepath=self.checkpoint_path,
+            save_weights_only=True,
+            verbose=1
+        )
 
         # Used to count when to update target network with main network's weights
         self.target_update_counter = 0
 
+        tf.config.gpu.set_per_process_memory_fraction(GPU_MEMORY_FRACTION)
+        tf.config.gpu.set_per_process_memory_growth(True)
+
+        if load_data:
+            self.restore_model()
+
+    def restore_model(self,path=None):
+
+        if path == None:
+            path = self.checkpoint_path
+
+        try:
+            self.policy_model.load_weights(path)
+            self.target_model.set_weights(self.policy_model.get_weights())
+
+        except Exception:
+            print("Could not load weights")
+
 
     def create_model(self):
-        model = Sequential()
+        model = keras.models.Sequential([
+            keras.layers.Conv2D(256, (3, 3), input_shape=self.input_size),
+            keras.layers.Activation('relu'),
+            keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            keras.layers.Dropout(0.2),
+            keras.layers.Flatten(),
+            keras.layers.Dense(64),
+            keras.layers.Dense(self.output_size, activation='linear')
+        ])
 
-        model.add(Conv2D(256, (3, 3),
-                         input_shape=self.input_size))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(256, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.2))
-
-        model.add(Flatten())  # Flatten 3D feature maps to 1D feature vectors
-        model.add(Dense(64))
-
-        model.add(Dense(self.output_size, activation='linear'))  # output_size=size of action space
-        model.compile(loss="mse", optimizer=Adam(lr=LEARNING_RATE), metrics=['accuracy'])
+        model.compile(optimizer=keras.optimizers.Adam(lr=0.001),
+                      loss='mse',
+                      metrics=['accuracy'])
         return model
 
 
@@ -122,22 +115,24 @@ class DQNAgent:
 
 
     # Trains main network every step during episode
-    def train(self, terminal_state, step):
+    def train_step(self, terminal_state, step):
+
         # Start training only if certain number of samples is already saved
         if len(self.replay_memory) < MIN_EXPERIENCE_REPLAY_SIZE:
             return
 
-        # Get a minibatch of random samples from memory replay table
+        # Get a minibatch of random samples from experience replay table
         minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
 
-        # Get current states from minibatch, then query NN model for Q values
-        current_states = np.array([transition[0] for transition in minibatch]) / 255
-        current_qs_list = self.model.predict(current_states)
+        # Get current states from minibatch, then query policy network
+        # for Q values
+        current_states = np.array([entry[0] for entry in minibatch]) / 255
+        current_qs_list = self.policy_model.predict(current_states)
 
-        # Get future states from minibatch, then query NN model for Q values
-        # When using target network, query it, otherwise main network should be queried
-        new_current_states = np.array([transition[3] for transition in minibatch]) / 255
-        future_qs_list = self.target_model.predict(new_current_states)
+        # Get future states from minibatch, then query target model for Q values
+        # for future Q values
+        future_states = np.array([entry[3] for entry in minibatch]) / 255
+        future_qs_list = self.target_model.predict(future_states)
 
         X = []
         y = []
@@ -145,8 +140,7 @@ class DQNAgent:
         # Now we need to enumerate our batches
         for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
 
-            # If not a terminal state, get new q from future states, otherwise set it to 0
-            # almost like with Q Learning, but we use just part of equation here
+            # Implement the bellman equation taking into account a discounted max future reward
             if not done:
                 max_future_q = np.max(future_qs_list[index])
                 new_q = reward + GAMMA * max_future_q
@@ -157,13 +151,13 @@ class DQNAgent:
             current_qs = current_qs_list[index]
             current_qs[action] = new_q
 
-            # And append to our training data
+            # Append features and targets to our training data
             X.append(current_state)
             y.append(current_qs)
 
         # Fit on all samples as one batch, log only on terminal state
-        self.model.fit(np.array(X) / 255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False,
-                       callbacks=[self.tensorboard] if terminal_state else None)
+        self.policy_model.fit(np.array(X) / 255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False,
+                              callbacks=[self.tensorboard,self.cp_callback] if terminal_state else [self.cp_callback])
 
         # Update target network counter every episode
         if terminal_state:
@@ -171,10 +165,11 @@ class DQNAgent:
 
         # If counter reaches set value, update target network with weights of main network
         if self.target_update_counter > UPDATE_TARGET_EVERY:
-            self.target_model.set_weights(self.model.get_weights())
+            self.target_model.set_weights(self.policy_model.get_weights())
             self.target_update_counter = 0
 
 
     # Queries main network for Q values given current observation space (environment state)
     def get_qs(self, state):
-        return self.model.predict(np.array(state).reshape(-1, *state.shape) / 255)[0]
+        return self.policy_model.predict(np.array(state).reshape(-1, *state.shape) / 255)[0]
+
